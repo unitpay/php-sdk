7a85e5e948561924c6a0b2276fdf629e7fbfe754
Dmitry V. Alexeev <mail@alexeevdv.ru>
2019-03-15 22:40:47 +0300

    Put each class in separate file

--- src/UnitPay.php
+++ src/UnitPay.php
@@ -21,55 +21,6 @@
  *
  */
 
-
-/**
- * Value object for paid goods
- */
-class CashItem
-{
-    private $name;
-
-    private $count;
-
-    private $price;
-
-    /**
-     * @param string $name
-     * @param int $count
-     * @param float $price
-     */
-    public function __construct($name, $count, $price)
-    {
-        $this->name  = $name;
-        $this->count = $count;
-        $this->price = $price;
-    }
-
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @return int
-     */
-    public function getCount()
-    {
-        return $this->count;
-    }
-
-    /**
-     * @return float
-     */
-    public function getPrice()
-    {
-        return $this->price;
-    }
-}
-
 /**
  * Payment method UnitPay process
  *
