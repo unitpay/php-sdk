d586b7af54c43d9349d2a50768692f19c211ae32
Alexander Ostroverkhov <alexruw@gmail.com>
2022-02-04 09:09:52 +0300

    Изменен список IP адресов unitpay

--- UnitPay.php
+++ UnitPay.php
@@ -150,7 +150,6 @@ class UnitPay
     private $supportedPartnerMethods = ['check', 'pay', 'error'];
     private $supportedUnitpayIp = [
         '31.186.100.49',
-        '178.132.203.105',
         '52.29.152.23',
         '52.19.56.234',
         '127.0.0.1' // for debug
