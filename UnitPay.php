77c7b9393e71b520ce26518fbd8b687ae07197c0
Feralonso <87639285+rakomissarenko@users.noreply.github.com>
2025-05-14 16:42:24 +0300

    Added new IP

--- UnitPay.php
+++ UnitPay.php
@@ -150,6 +150,7 @@ class UnitPay
     private $supportedPartnerMethods = ['check', 'pay', 'error'];
     private $supportedUnitpayIp = [
         '31.186.100.49',
+        '51.250.20.9',
         '52.29.152.23',
         '52.19.56.234',
         '127.0.0.1' // for debug
